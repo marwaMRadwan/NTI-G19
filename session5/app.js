4b799c1484d00b12129cc14e9fd34b7dfd06bab6
marwaMRadwan <marwaradwan872@hotmail.com>
2022-06-12 13:32:47 +0200

    s5

--- session5/app.js
+++ session5/app.js
@@ -1,5 +1,48 @@
 const user = require("./controller/user")
-// user.addUser("marwa", 37, "marwa@gmail.com")
-// user.readAll()
 // user.singleUser("99hn349cl4b2f9e")
-user.editUser("99hn349cl4b2nf9e", {name:"a", age:"c"})
+// user.editUser("99hn349cl4b2nf9e", {name:"a", age:"c"})
+// console.log(process.argv)
+// user.addUser(process.argv[2], 37, "marwa@gmail.com")
+//yargs
+const yargs = require('yargs')
+
+yargs.command({
+    command:"showAll",
+    handler: function(){
+        user.readAll()
+    }
+})
+
+yargs.command({
+    command:"addUser",
+    builder:{
+        name:{
+            type:String,
+            demandOption:true
+        },
+        email:{},
+        age:{
+            type:Number,
+            min:21
+        }
+    },
+    handler: function(argv){
+        user.addUser(argv.name, argv.age, argv.email)
+    }
+})
+
+yargs.argv
+
+
+
+
+
+
+
+
+
+
+
+
+
+
