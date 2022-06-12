0865b9606ff0c397cbb77e38b36f16ccffd74ef8
marwaMRadwan <marwaradwan872@hotmail.com>
2022-06-12 14:52:43 +0200

    s5

---
+++ session5/apiReq.js
@@ -0,0 +1,14 @@
+//http http2 https
+const https = require("https")
+const url = "https://jsonplaceholder.typicode.com/posts"
+const req = https.request(url, (res)=>{
+    let allData = ""
+    res.on("data", (myData)=>{
+        allData+= myData.toString()
+    })
+    res.on("end", ()=>{
+        console.log(JSON.parse(allData))
+    })
+})
+req.on("error", (err)=> console.log(`error ${err}`))
+req.end()
