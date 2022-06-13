dcd030cc5ae5b1f6a0f12e8e94e72c38d6c4ae05
marwaMRadwan <marwaradwan872@hotmail.com>
2022-06-13 14:41:25 +0200

    s6

---
+++ session6/postsApp/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const hbs = require("hbs")
+const path = require("path")
+
+const app = express()
+
+const postRoutes = require("../routes/post.routes")
+
+app.set('view engine', 'hbs')
+
+app.use(express.static(path.join(__dirname, "../resources/public")))
+app.set('views', path.join(__dirname, "../resources/views"))
+hbs.registerPartials(path.join(__dirname, "../resources/layouts"))
+
+app.use(postRoutes)
+
+module.exports = app
