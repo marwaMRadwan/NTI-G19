75e11fdb1a5f50def21721a389649c4359fefba3
marwaMRadwan <marwaradwan872@hotmail.com>
2022-06-14 08:54:36 +0200

    s6

--- session6/postsApp/index.js
+++
@@ -1,3 +0,0 @@
-const app = require("./src/app")
-const PORT = 3000
-app.listen(PORT, ()=> console.log(`server on http://localhost:${PORT}`))
--- session6/postsApp/routes/post.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router()
-const postController = require("../controllers/post.controller")
-router.get("/", postController.home)
-router.get("/add", postController.add)
-router.get("/single",postController.single)
-router.get("/edit",postController.edit)
-module.exports = router
