4c3562f29522019342d17d661f87e654b92b58b5
marwaMRadwan <marwaradwan872@hotmail.com>
2022-06-14 10:35:11 +0200

    s7

--- session6-7/postsApp/controllers/post.controller.js
+++ session6-7/postsApp/controllers/post.controller.js
@@ -1,15 +1,23 @@
 class Post{
     static home = (req, res)=> { 
-        res.render("home")
+        res.render("home", {
+            pageTitle:"all posts"
+        })
     }
     static add = (req, res)=> { 
-        res.render("add")
+        res.render("add", {
+            pageTitle:"Add Post"
+        })
     }
     static single = (req, res)=> { 
-        res.render("single")
+        res.render("single", {
+            pageTitle:"single Post"
+        })
     }
     static edit =  (req, res)=> { 
-        res.render("edit")
+        res.render("edit", {
+            pageTitle:"Edit Post"
+        })
     }
 }
 module.exports = Post
